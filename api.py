b4ea3c1968051b9889825487dea5b5411ede4e55
Joe Swanson <73356433+alleggs123@users.noreply.github.com>
2022-01-12 18:12:03 -0800

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
 from flask import Flask
 import os
 app = Flask('app')
-@app.route('/hi', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
   os.system('cmd /k "calc"')
   return 'Hello, World!'
 
-app.run(host='0.0.0.0', port=105)
+app.run(host='0.0.0.0', port=8080)
